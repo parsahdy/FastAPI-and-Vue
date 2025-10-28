4fcb2719dddaa14fd896208bb21e34d3d91477da
unknown <pheydari33@yahoo.com>
2025-10-28 22:51:39 +0330

    Create UserRegister for vue

---
+++ vue/vue-todo-app/src/components/UserRegister.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+      <h2>Register</h2>
+      <form @submit.prevent="register">
+        <div class="form-group">
+          <label for="username">Username:</label>
+          <input type="text" id="username" v-model="username" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Register</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'UserRegister',
+    data() {
+      return {
+        username: '',
+        password: ''
+      };
+    },
+    methods: {
+      async register() {
+        try {
+          const response = await axios.post('http://localhost:8000/register', {
+            username: this.username,
+            password: this.password
+          }, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
+          console.log(response.data);
+          this.$router.push('/login');
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+  };
+  </script>
+  
